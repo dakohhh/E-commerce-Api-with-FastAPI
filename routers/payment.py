20b4b46714ce689cb24dfb05d58018f016ceec64
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-03-07 03:30:01 -1200

    cleaned javascript code and payment issues

--- routers/payment.py
+++ routers/payment.py
@@ -1,7 +1,8 @@
 import os
 import stripe
-from fastapi import APIRouter, Request
-from response.response import flash
+from fastapi import APIRouter, Request, status
+from exceptions.custom_execption import ServerErrorException
+from response.response import flash, customResponse
 from dotenv import load_dotenv
 
 
@@ -16,25 +17,28 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 @payment.post("/create-payment-session")
 async def create_payment_session(amount: float, currency:str ,order_no:str, request:Request):
-    session = stripe.checkout.Session.create(
-        payment_method_types=["card"],
-        line_items=[
-            {
-                "price_data": {
-                    "currency": currency,
-                    "product_data": {
-                        "name": f"ORDER_NO:{order_no}",
+    try:
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": currency,
+                        "product_data": {
+                            "name": f"ORDER_NO:{order_no}",
+                        },
+                        "unit_amount": round(amount * 100),
                     },
-                    "unit_amount": round(amount * 100),
+                    "quantity": 1,
                 },
-                "quantity": 1,
-            },
-        ],
-        mode="payment",
-        success_url=request.url_for("payment_successful", order_no=order_no),
-        cancel_url=request.url_for("payment_error", order_no=order_no),
-    )
-    return {"session_id": session.id}
+            ],
+            mode="payment",
+            success_url=request.url_for("payment_successful", order_no=order_no),
+            cancel_url=request.url_for("payment_error", order_no=order_no),
+        )
+        return customResponse(status.HTTP_200_OK, "Session ID Created", data={"session_id":session})
+    except:raise ServerErrorException("Connection Problem")
+
 
 
 
